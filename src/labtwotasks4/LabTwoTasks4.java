0ad7ad5007ccbfc179ceb7c1c44fe2d3d0611454
USER <USER@10.52.33.237>
2019-02-01 14:39:54 +0300

    LabTwoTasks4-113453

---
+++ src/labtwotasks4/LabTwoTasks4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labtwotasks4;
+
+/**
+ *
+ * @author USER
+ */
+public class LabTwoTasks4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double temperatureInFahrenheit=212;
+        double temperatureInCelsius=(temperatureInFahrenheit-32)*5/9;
+        
+ System.out.println("212.0 degree Fahrenheit is equal to "+temperatureInCelsius+" in Celsius");
+    }
+    
+}
